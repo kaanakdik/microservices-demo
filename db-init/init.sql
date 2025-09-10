cdc1c88593e0ef0dd21814f5bf547731d025d1a4
kaanakdik <kaanakdik@gmail.com>
2025-09-10 20:33:40 +0300

    init.db eklendi

---
+++ db-init/init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS microservices;
+
+USE microservices;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (name, email) VALUES
+('Kaan', 'kaan@example.com'),
+('Ahmet', 'ahmet@example.com'),
+('Ayşe', 'ayse@example.com');
